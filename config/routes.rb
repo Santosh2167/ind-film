62395d39c49d5213569462f58d988ea51fa24825
Santosh <santosh.poudyal@gmail.com>
2018-10-14 22:16:39 +1100

    created model, routes and new.html.erb

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,22 @@
 Rails.application.routes.draw do
+  # get 'movies/new'
+  # get 'movies/create'
+  # get 'movies/update'
+  # get 'movies/edit'
+  # get 'movies/index'
+  # get 'movies/show'
+  # get 'movies/destroy'
+  
+  post "movies",to:"movies#create"
+  get "movies/new", to:"movies#new"
+
+  patch "movies/:id",to:"movies#update"
+  put "movies/:id",to:"movies#update"
+  get "movies/:id/edit", to:"movies#edit"
+
+  get "movies", to:"movies#index"
+  get "movies/:id",to:"movies#show"
+  
+  destroy "movies/:id",to:"movies#destroy"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
